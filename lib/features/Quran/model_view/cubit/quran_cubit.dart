f6fb558e8757bfc3a5bc9e23db017da82a90dd86
Mohamed-Samirr <ms954628@gmail.com>
2025-10-02 18:58:48 +0300

    quran update

---
+++ lib/features/Quran/model_view/cubit/quran_cubit.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:depiproject/features/Quran/models/quran_model.dart';
+
+part 'quran_state.dart';
+
+class QuranCubit extends Cubit<QuranState> {
+  QuranCubit() : super(QuranInitial());
+
+  List<Surah> quranSurahs = [];
+
+  Future<void> loadQuranData() async {
+    emit(QuranLoading());
+    try {
+      // Load surahs information
+      final String surahsJsonString = await rootBundle.loadString('assets/json/surahs.json');
+      final List<dynamic> surahsData = json.decode(surahsJsonString);
+      
+      quranSurahs = [];
+      for (var surahData in surahsData) {
+        try {
+          quranSurahs.add(Surah.fromJson(surahData));
+        } catch (e) {
+          print('Error parsing surah ${surahData['id']}: $e');
+        }
+      }
+      
+      emit(QuranLoaded(quranSurahs));
+    } catch (e) {
+      print('Error loading Quran data: $e');
+      emit(QuranError('فشل في تحميل بيانات القرآن الكريم: ${e.toString()}'));
+    }
+  }
+  
+  Future<List<Verse>> loadSurahVerses(int surahId) async {
+    try {
+      final String versesJsonString = await rootBundle.loadString('assets/json/quran.json');
+      final Map<String, dynamic> versesData = json.decode(versesJsonString);
+      
+      List<Verse> verses = [];
+      if (versesData.containsKey(surahId.toString())) {
+        final List<dynamic> surahVerses = versesData[surahId.toString()];
+        for (var verseData in surahVerses) {
+          verses.add(Verse.fromJson(verseData));
+        }
+      }
+      
+      return verses;
+    } catch (e) {
+      print('Error loading verses for surah $surahId: $e');
+      return [];
+    }
+  }
+}
---
+++ lib/features/Quran/model_view/cubit/quran_state.dart
@@ -0,0 +1,19 @@
+part of 'quran_cubit.dart';
+
+abstract class QuranState {}
+
+class QuranInitial extends QuranState {}
+
+class QuranLoading extends QuranState {}
+
+class QuranLoaded extends QuranState {
+  final List<Surah> surahs;
+  
+  QuranLoaded(this.surahs);
+}
+
+class QuranError extends QuranState {
+  final String message;
+  
+  QuranError(this.message);
+}
