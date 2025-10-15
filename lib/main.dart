f9cac74ac6b25cf59d1413266e0ab1a0c015e8e9
Beso-07 <mahmoudbassem34@gmail.com>
2025-10-15 19:53:42 +0300

    initalize local notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:depiproject/core/services/location_service.dart';
+import 'package:depiproject/core/widgets/time_zone.dart';
 import 'package:depiproject/features/splash/views/splash_view.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocationService.getCurrentLocation();
-   runApp(DevicePreview(builder: (context) => const IslamicApp()));
+  await initTimeZone();
+
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  var initializationSettingsAndroid =
+      const AndroidInitializationSettings('@mipmap/ic_launcher');
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  runApp(DevicePreview(builder: (context) => const IslamicApp()));
 }
 
 class IslamicApp extends StatelessWidget {
