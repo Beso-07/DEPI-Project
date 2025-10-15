d07c6dd81b58e211e1b2a0d9c8b3fa736df86877
Beso-07 <mahmoudbassem34@gmail.com>
2025-10-15 17:29:15 +0300

    qiblah last update

--- lib/features/qiblah/views/qiblah_view.dart
+++ lib/features/qiblah/views/qiblah_view.dart
@@ -1,85 +1,60 @@
-import 'package:depiproject/features/qiblah/views/widgets/compass_contraller.dart';
+import 'dart:math' as math;
+import 'package:depiproject/core/constants/assets.dart';
+import 'package:depiproject/core/widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
-import 'package:get/get_state_manager/src/simple/get_state.dart';
 
-class QiblahView extends StatelessWidget {
+class QiblahView extends StatefulWidget {
   const QiblahView({super.key});
+
+  @override
+  State<QiblahView> createState() => _QiblahViewState();
+}
+
+class _QiblahViewState extends State<QiblahView> {
+  double? _direction;
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+        _direction = event.heading;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Qiblah View'),
-      ),
-      body: GetBuilder<CompassContraller>(
-        init: CompassContraller(),
-        builder: (controller) {
-          return controller.hasPermissions
-              ? StreamBuilder(
-                  stream: FlutterCompass.events,
-                  builder: (context, AsyncSnapshot<CompassEvent> snapshot) {
-                    if (snapshot.hasError ||
-                        snapshot.connectionState == ConnectionState.waiting ||
-                        snapshot.data == ConnectionState.none) {
-                      return Text('wait');
-                    } else {
-                      if (snapshot.data!.heading == null) {
-                        return const Center(
-                          child: Text("Device does not have sensors !"),
-                        );
-                      } else {
-                        return Column(
-                          children: [
-                            Text("${snapshot.data!.heading}°"),
-                            Material(
-                              shape: const CircleBorder(),
-                              clipBehavior: Clip.antiAlias,
-                              elevation: 4,
-                              child: Container(
-                                padding: const EdgeInsets.all(16),
-                                alignment: Alignment.center,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Transform.rotate(
-                                  angle: ((snapshot.data!.heading ?? 0) *
-                                      (3.14159265359 / 180) *
-                                      -1),
-                                  child: Image.asset(
-                                    'assets/images/image.png',
-                                    color: Colors.red,
-                                    fit: BoxFit.contain,
-                                    width: double.infinity,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        );
-                      }
-                    }
-                  },
-                )
-              : Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      controller.requestPermission();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 24, vertical: 12),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12)),
-                    ),
-                    child: const Text(
-                      'طلب صلاحية الموقع',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                );
-        },
+      body: Column(
+        children: [
+          const MainAppBar(title: 'القبلة'),
+          const SizedBox(
+            height: 80,
+          ),
+
+          Column(
+            children: [
+              Text(
+                "Degree: ${_direction == null ? 0 : _direction!.toInt()}°",
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text("---------------------"),
+            ],
+          ),
+
+          // 🧭 Compass
+          Transform.rotate(
+            angle: ((_direction ?? 0) * (math.pi / 180) * -1),
+            child: Image.asset(
+              Imagespath.compass,
+              width: 250,
+            ),
+          ),
+        ],
       ),
     );
   }
